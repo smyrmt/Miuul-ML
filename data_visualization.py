a3092ad42775c5a2c5a0b1841341ee65a15973cf
smyrmt <73067248+smyrmt@users.noreply.github.com>
2022-07-14 11:13:26 +0300

    Create data_visualization.py

---
+++ data_visualization.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import seaborn as sns
+import  matplotlib.pyplot as plt
+pd.set_option("display.max_columns", None)
+pd.set_option("display.width",500)
+df = sns.load_dataset("titanic")
+df.head()
+
+df["sex"].value_counts().plot(kind="bar")
+plt.show()
