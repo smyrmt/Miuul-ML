30f21ee1e76be844c147737c3695e16ec443c0a9
smyrmt <73067248+smyrmt@users.noreply.github.com>
2022-07-15 15:47:27 +0300

    Update data_analysis.py

--- data_analysis.py
+++ data_analysis.py
@@ -100,6 +100,15 @@ def num_summary(dataframe, num_col, plot=False):
 num_summary(df, "fare", True)
 
 ########################## Değişkenlerin Yakalanması  ########################################
+   
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+pd.set_option("display.max_columns", None)
+pd.set_option("display.width",500)
+df = sns.load_dataset("titanic")
+
 #docstring
 
 def grab_col_names(dataframe, cat_th=10, car_th=20):
@@ -130,6 +139,25 @@ def grab_col_names(dataframe, cat_th=10, car_th=20):
     num_but_cat cat_cols'un içerisinde.
 
     """
+    cat_cols = [col for col in df.columns if str(df[col].dtype) in ["category", "bool", "object"]]
+    # sayısal gözüken ama kategorik olan değişkenler
+    num_but_cat = [col for col in df.columns if df[col].nunique() < 10 and df[col].dtypes in ["int64", "float64"]]
+    # kategorik gözüken ama kategorik olmayan çok sayıda farklı değere sahip değişkenler (örneğin isim-soyisim gibi)
+    cat_but_car = [col for col in df.columns if (df[col].nunique() > 20) and (str(df[col].dtype) in ["category", "bool", "object"])]
 
-help(grab_col_names)
-    
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in df.columns if col not in cat_but_car]  # kategorik olmayan değişkenleri verir
+
+    num_cols = [col for col in df.columns if df[col].dtypes in ["float", "int"]]  # sayısal değişkenlerin tümünü verir
+    num_cols = [col for col in num_cols if col not in cat_cols]  # sayısal değişkenlerden gerçekte kategorik olanlarını ayrıştırarak gerçek sayısal değişkenleri verir
+
+    print(f"Observations: {dataframe.shape[0]}")
+    print(f"Variables: {dataframe.shape[1]}")
+    print(f"cat_cols: {len(cat_cols)}")
+    print(f"num_cols: {len(num_cols)}")
+    print(f"cat_but_car: {len((cat_but_car))}")
+    print(f"num_but_cat: {len((num_but_cat))}")
+
+    return cat_cols, num_cols, cat_but_car
+
+grab_col_names(df)
